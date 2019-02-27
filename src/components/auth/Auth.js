be881d30920f0a610c8edec116609c67c0b2e504
popejb1116 <popejb1116@gmail.com>
2019-02-27 14:32:56 -0500

    Converted Sign In to independent page - Pre Materalize

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -2,110 +2,13 @@ import React, { Component }from 'react';
 import SignIn from './SignIn'
 import SignUp from './SignUp'
 //import firebase from '../../config/fbConfig'
-import { auth, firestore } from '../../config/fbConfig'
+//import { auth, firestore } from '../../config/fbConfig'
 
 class Auth extends Component {
 
-  state = {
-    email_SI: '',
-    password_SI: '',
-
-    email_SU: '',
-    firstName: '',
-    lastName: '',
-    password_SU: '',
-
-    errorMessage: ''
-  }
-
-  /* componentDidMount(){
-    auth.onAuthStateChanged(user => {
-      if (user) {
-        console.log('Auth cdm: user')
-      } else {
-        console.log('Auth cdm: NO user')
-      }
-    })
-  } */
-
-  handleChange = (event) => {
-    const { id, value } = event.target
-    this.setState({
-      [id]: value
-    })
-  }
-
-  handleSubmit = (event) => {
-
-    event.preventDefault()
-    let { id } = event.target
-
-    switch (id) {
-
-      // Handle user sign in
-      case 'SignIn':
-        auth.signInWithEmailAndPassword(
-          this.state.email_SI,
-          this.state.password_SI
-        )
-        .then(() => {
-          this.props.history.push('/forum')
-        })
-        .catch(err => {
-          this.handleErrorAndClearForm(err.message)
-        })
-        break
-
-      // Handle user sign up, add profile to users collection
-      case 'SignUp':
-        auth.createUserWithEmailAndPassword(
-          this.state.email_SU,
-          this.state.password_SU
-        )
-        .then(res => {
-          // aggregate user profile data for .set()
-          const { uid } = res.user
-          const userProfile = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            createdAt: new Date()
-          }          
-          // This is returned to the outer Promise and picks up at the next .then
-          return firestore.collection('users').doc(uid).set(userProfile)
-        })
-        .then(() => {
-          //console.log('New user profile added')
-          this.props.history.push('/forum')
-        })
-        .catch(err => {
-          this.handleErrorAndClearForm(err.message)
-        })
-        break
-    
-      default:
-        const defaultError = 'Non-Firebase Error'
-        this.handleErrorAndClearForm(defaultError)
-        break
-    }
-  }
-
-  handleErrorAndClearForm = error => {
-    this.setState({
-      email_SI: '',
-      password_SI: '',
-
-      email_SU: '',
-      firstName: '',
-      lastName: '',
-      password_SU: '',
-
-      errorMessage: error
-    })
-  }
-
   render(){
 
-    let errorElement = this.state.errorMessage ? 
+    /* let errorElement = this.state.errorMessage ? 
     (
       <div className="error-container">
         <div className="auth-error">
@@ -114,7 +17,7 @@ class Auth extends Component {
       </div>
     ) : (
       null
-    )
+    ) */
 
     return (
       <div className="Auth">
@@ -122,18 +25,17 @@ class Auth extends Component {
           <div className="form-container">
 
             <div className="box signin">
-              <h3>Sign In</h3>          
-              <SignIn formValues={this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} handleError={this.handleError} />
+              <SignIn history={this.props.history} />
             </div>
 
-            <div className="box signup">
+            {/* <div className="box signup">
               <h3>Sign Up</h3>
               <SignUp formValues={this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} handleError={this.handleError} />
             </div>
-
+ */}
           </div>
         </div>
-        {errorElement}
+        {/*errorElement*/}
       </div>
     )
   }
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,22 +1,77 @@
-import React from 'react';
+import React from 'react'
+import { auth } from '../../config/fbConfig'
+//import M from 'materialize-css'
 
-const SignIn = (props) => {
+class SignIn extends React.Component {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = event => {
+    const { id, value } = event.target
+    this.setState({
+      [id]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const { email, password } = this.state    
+    auth.signInWithEmailAndPassword(email, password)
+    .then(user => {
+      console.log(user)
+      this.props.history.push('/forum')
+    })
+    .catch(error => {
+      this.handleError(error)
+    })
+  }
+
+  handleError = error => {
+    console.log(error.message)
+    
+    /* var toastHTML = 
+      '<span>I am toast content</span>' +
+      '<button className="btn-flat toast-action onClick={this.dismissToast}">Dismiss</button>';
+    M.toast({html: toastHTML}); */
+    
+    this.setState({
+      email: '',
+      password: ''
+    })
+  }
+
+  dismissToast(){
+    console.log('dismiss toast')
+  }
+
+  render(){
+
+    return (
+      <form className="signin-form" onSubmit={this.handleSubmit} id="SignIn">        
+          <div className="input-field">
+            <label htmlFor='email'>Email Address</label>
+            <input id="email" type="email" value={this.state.email} onChange={this.handleChange}/>
+          </div>
+          <div className="input-field">
+            <label htmlFor='password'>Password</label>
+            <input id="password" type="password" value={this.state.password} onChange={this.handleChange}/>
+          </div>
+          <div className="center">
+            <button className="btn-large z-depth-0">Sign In</button>
+          </div>
+
+          <div className="signup-block">
+            <div className="signup-message">Don't have an account?</div>
+            <div className="btn z-depth-0">Sign Up</div>
+          </div>          
+      </form>
+    )
+  }
   
-  return (
-    <form onSubmit={props.handleSubmit} id="SignIn">        
-        <div className="input-field">
-          <label htmlFor='email_SI'>Email Address</label>
-          <input id="email_SI" type="email" value={props.formValues.email_SI} onChange={props.handleChange}/>
-        </div>
-        <div className="input-field">
-          <label htmlFor='password_SI'>Password</label>
-          <input id="password_SI" type="password" value={props.formValues.password_SI} onChange={props.handleChange}/>
-        </div>
-        <div className="center">
-          <button className="btn-large z-depth-0">Sign In</button>
-        </div> 
-    </form>
-  )
 }
 
 
