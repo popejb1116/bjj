f8245af3c24b4fc84a824ce2e63d5fea18cd9f9d
popejb1116 <popejb1116@gmail.com>
2019-03-06 16:09:17 -0500

    Implemented styled-components on ProfileForm.js

--- src/components/auth/signUp/ProfileForm.js
+++ src/components/auth/signUp/ProfileForm.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 
-const StyledForm = styled.div`
+const StyledModalHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 2rem 0 2rem;
   color: white;
   background-color: transparent;
-  padding: 5%;
+  font-size: 2rem;
+`
 
-  .form-header{
-    text-align: center;
-  }
+const StyledModalTextFieldsBlock = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  color: white;
+  background-color: transparent;
+  font-size: 1rem;
+  margin: 1rem;
+  padding: 1rem;
 
   label, input {
      color: white;
@@ -20,132 +30,161 @@ const StyledForm = styled.div`
   box-shadow: 0 1px 0 0 #ff1744 !important;
   font-size: 35px;
   }
-
-  .form-nav {
-    display: flex;
-    justify-content: center;
-    margin-top: 1.5rem;
-  }
-
-  .btn-large, .btn {
-    background-color: transparent;
-
-    .material-icons{
-      font-size: 60px;
-    }
-  }
-
-  .btn-large:hover, .btn:hover{
-    color: white;
-    background-color: #ff1744;
-  }
 `
 
-const StyledRadio = styled.div`
+const StyledRadioGroup = styled.div`
 
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  margin-top: 1.5rem;
+  color: white;
+  background-color: transparent;
+  font-size: 1rem;
+  margin: 1rem;
+  padding: 1rem;
 
   .radio-title{
-    margin-right: 10px;
+    color: white;
+    margin-right: 1rem;
+    font-size: 1rem;
+    min-width: 8rem;
   }
 
-  [type="radio"]:not(:checked)+span{
+  .radio-option{
+    min-width: 6rem;
+
+    [type="radio"]:not(:checked)+span{
+    color: white;
     padding-left: 25px;
     margin-right: 25px;
+    }
+
+    [type="radio"]:checked+span{
+      color: white;
+      padding-left: 25px;
+      margin-right: 25px;
+    }
+
+    [type="radio"]:checked+span:after{
+      background-color: #ff1744;
+      border: 2px;
+      border-color: #ff1744;
+    }
+  }  
+`
+
+const StyledModalNav = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+
+  .btn-large {
+    color: white;
+    background-color: transparent;
+    margin: 0rem 0.5rem;
+    font-weight: bold;
   }
 
-  [type="radio"]:checked+span{
-    padding-left: 25px;
-    margin-right: 25px;
-  }
-
-  [type="radio"]:checked+span:after{
+  .btn-large:hover{
     background-color: #ff1744;
-    border: 2px;
-    border-color: #ff1744;
+    margin: 0rem 0.5rem;
+    font-weight: bold;
+  }
+`
+
+const StyledModalFooter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  
+  .btn {
+    color: white;
+    background-color: transparent;
+  }
+
+  .btn:hover{
+    background-color: #ff1744;
   }
 `
 
 const ProfileForm = props => {
   return (
-    <StyledForm>
+    <Fragment>
       <form > 
 
-        <div className="form-header">
-          <h5>Now Your Profile And Martial Arts History...</h5>
-        </div>        
+        <StyledModalHeader>
+          Now Your Profile And Martial Arts History...  
+        </StyledModalHeader>      
 
-        <div className="input-field">
-          <label htmlFor='firstName'>First Name</label>
-          <input id="firstName" type="text" value={props.state.firstName} onChange={props.handleChange}/>
-        </div>
+        <StyledModalTextFieldsBlock>
+          <div className="input-field">
+            <label htmlFor='firstName'>First Name</label>
+            <input id="firstName" type="text" value={props.state.firstName} onChange={props.handleChange}/>
+          </div>
+          
+          <div className="input-field">
+            <label htmlFor='lastName'>Last Name</label>
+            <input id="lastName" type="text" value={props.state.lastName} onChange={props.handleChange}/>
+          </div>    
+        </StyledModalTextFieldsBlock>        
         
-        <div className="input-field">
-          <label htmlFor='lastName'>Last Name</label>
-          <input id="lastName" type="text" value={props.state.lastName} onChange={props.handleChange}/>
-        </div>    
-        
-        <StyledRadio>
-          <div className="radio-title">Primary Discipline</div>
-          <label htmlFor="bjj">
+        <StyledRadioGroup>
+          <div className="radio-title">Primary</div>
+          
+          <label className="radio-option" htmlFor="bjj">
             <input name="primaryDiscipline" id="bjj" type="radio" onChange={props.handleChange} />
             <span>BJJ</span>
           </label>
           
-          <label htmlFor="judo">
+          <label className="radio-option" htmlFor="judo">
             <input name="primaryDiscipline" id="judo" type="radio" onChange={props.handleChange} />
             <span>Judo</span>
           </label>
 
-          <label htmlFor="muay">
+          <label className="radio-option" htmlFor="muay">
             <input name="primaryDiscipline" id="muay" type="radio" onChange={props.handleChange} />
             <span>Muay Thai</span>
           </label>        
 
-          <label htmlFor="other">
+          <label className="radio-option" htmlFor="other">
             <input name="primaryDiscipline" id="other" type="radio" onChange={props.handleChange} />
             <span>Other</span>
           </label>                
-        </StyledRadio>
+        </StyledRadioGroup>
 
-        <StyledRadio>
+        <StyledRadioGroup>
           <div className="radio-title">Years Experience</div>
-          <label htmlFor="zeroFour">
+
+          <label className="radio-option" htmlFor="zeroFour">
             <input name="yearsExperience" id="zeroFour" type="radio" value={props.state.yearsExperience} onChange={props.handleChange} />
             <span>0-4</span>
           </label>
           
-          <label htmlFor="fiveNine">
+          <label className="radio-option" htmlFor="fiveNine">
             <input name="yearsExperience" id="fiveNine" type="radio" value={props.state.yearsExperience} onChange={props.handleChange} />
             <span>5-9</span>
           </label>
 
-          <label htmlFor="tenPlus">
+          <label className="radio-option" htmlFor="tenPlus">
             <input name="yearsExperience" id="tenPlus" type="radio" value={props.state.yearsExperience} onChange={props.handleChange} />
             <span>10+</span>
-          </label>                   
-        </StyledRadio>
+          </label>
+        </StyledRadioGroup>
 
-        <div className="form-nav">
-          <a 
-            className="btn-large z-depth-0"
-            onClick={() => props.handleMode('credentials')}>
-            <i className="material-icons">arrow_left</i>
-          </a>      
-          <a
-            className="btn-large z-depth-0"
-            onClick={() => props.handleMode('review')}>
-            <i className="material-icons">arrow_right</i>
-          </a>          
-        </div>
+        <StyledModalNav>
+          <a className="btn-large z-depth-0" onClick={() => props.handleMode('credentials')}>Go Back</a>
+          <a className="btn-large z-depth-0" onClick={() => props.handleMode('review')}>Onward</a>
+        </StyledModalNav>
 
-        <a className="modal-close btn z-depth-0 right" href="/">Cancel</a>
+        <StyledModalFooter>
+        <a 
+          className="modal-close btn z-depth-0"
+          href="/signin"
+        >Cancel
+        </a>
+      </StyledModalFooter>
 
       </form>
-    </StyledForm>
+    </Fragment>
   )
 }
 
