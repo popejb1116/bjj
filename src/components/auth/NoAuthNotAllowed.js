7c45a41c99649c54df72858e12e8de08d863d9eb
Jonathan Pope <jonathanpope@Jonathans-MacBook-Pro.local>
2019-04-08 14:57:12 -0400

    Styled NoAuthNotAllowed component

--- src/components/auth/NoAuthNotAllowed.js
+++ src/components/auth/NoAuthNotAllowed.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import moe from '../../images/moe_grumpy.jpg'
+import styled from 'styled-components'
+
+const NoAuth = styled.div`
+   display: flex;
+   flex-flow: column nowrap;
+   align-items: center;
+   color: white;
+   background-color: rgb(0,0,0, 0.6);
+   padding-bottom: 1rem;
+
+   h3{
+      text-align: center;
+   }
+` 
 
 const NoAuthNotAllowed = () => {
    return (
-      <div className="container center">
-         <h1>Heyyyyy, What's the big idea?</h1>
-         <img src={moe} />
-      </div>
+      <NoAuth className="container">
+         <h3>Heyyyyy, What's the big idea?</h3>
+         <img src={moe} alt="" />
+      </NoAuth>
    )
 }
 
